5bf0e2315ec5d3ebf7cbf9a6e23412eb56783c84
Jimdooz <romain.saclier@gmail.com>
2020-02-11 18:54:21 +0100

    Add Example scene with Planet Generator

---
+++ Assets/Scripts/PlanetsGenerator/PlanetGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    [Range(-200f, 200f)]
+    public float speedRotation = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        gameObject.transform.Rotate(0,Time.deltaTime * speedRotation, 0);
+    }
+}
