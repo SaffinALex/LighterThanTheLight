7d1ec63873ed27aa0a8fe1968fa02af225212f26
Floran Chazelas <flo.chazelas@orange.fr>
2020-02-24 22:25:54 +0100

    Comportement Bot

--- Assets/Scripts/Entity/Vaisseau/EntitySpaceShipBehavior.cs
+++ Assets/Scripts/Entity/Vaisseau/EntitySpaceShipBehavior.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityBehavior : MonoBehaviour
+public class EntitySpaceShipBehavior : MonoBehaviour
 {
     private Rigidbody2D r2d;
+    public float life;
     public float speed;
+    //weapon associé à un type de bullet
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +20,35 @@ public class EntityBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (life <= 100)
+        {
+            Destroy(r2d);
+        }
 
+        move();
+    }
+
+    public void move()
+    {
         if (Input.GetKey("d"))
         {
-            r2d.velocity = new Vector2(200, 0);
+            r2d.velocity = new Vector2(speed, 0);
         }
         else if (Input.GetKey("q"))
         {
-            r2d.velocity = new Vector2(-200, 0);
+            r2d.velocity = new Vector2(-speed, 0);
         }
         else
         {
             r2d.velocity = new Vector2(0, 0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("playerBullet"))
+        {
+            life -= 100;
         }
     }
 }
