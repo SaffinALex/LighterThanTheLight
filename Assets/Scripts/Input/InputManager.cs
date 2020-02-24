f0013250abc904d65f1ab122616f7727b825b6a3
Romain Saclier <romain.saclier@etu.utmontpellier.fr>
2020-02-24 15:13:21 +0100

    InputSystem

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager {
+
+  // Dictionary for player -> InputSystem
+  // JSON representation
+  /*
+    {
+      0 : [Input1, Input2],
+      1 : [Input2, Input3]
+    }
+  */
+  private static Dictionary<int, List<InputSystem> > dicoInputSystem = new Dictionary<int, List<InputSystem> >();
+
+  /*
+    Return an input by the name and the player value
+  */
+  public static float GetInput(string inputName, int player = 0){
+    if(!PlayerExist(player)) return 0;
+    float value = 0;
+    //Get the max value distance from 0
+    for(int i = 0; i < dicoInputSystem[player].Count; i++){
+      float valueGet = dicoInputSystem[player][i].GetInput(inputName);
+      if(System.Math.Abs(valueGet) > System.Math.Abs(value)) value = valueGet;
+    }
+    return value;
+  }
+
+  private static bool PlayerExist(int player = 0){
+    return dicoInputSystem.ContainsKey(player);
+  }
+
+  private static void initPlayer(int player){
+    if(!PlayerExist(player)) dicoInputSystem.Add(player, new List<InputSystem>());
+  }
+
+  public static bool Subscribe(InputSystem s, int player = 0){
+    initPlayer(player);
+    dicoInputSystem[player].Add(s);
+    return true;
+  }
+
+  public static bool Unsubscribe(InputSystem s, int player = 0){
+    if(!PlayerExist(player)) return false;
+    int count = dicoInputSystem[player].Count;
+    dicoInputSystem[player].Remove(s);
+    return count > dicoInputSystem[player].Count;
+  }
+}
---
+++ Assets/Scripts/Input/InputSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour {
+  protected Dictionary<string, float> inputValues = new Dictionary<string, float>();
+
+  private bool InputExist(string inputName){
+    return inputValues.ContainsKey(inputName);
+  }
+
+  public float GetInput(string inputName){
+    if(!InputExist(inputName)) return 0;
+    return inputValues[inputName];
+  }
+}
