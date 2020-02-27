63bcf1adbf4aec05e8195305ebbef9be6b79abb8
Razania <j.ferreira1215@gmail.com>
2020-02-27 09:27:10 +0100

    TestHealthBar

--- Assets/Scripts/UI/HealthBarControl.cs
+++ Assets/Scripts/UI/HealthBarControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBarControl : MonoBehaviour
 {
@@ -24,11 +25,11 @@ public class HealthBarControl : MonoBehaviour
     {
         if (Input.GetKey("up"))
         {
-            setHealthPercent(this.healthPercent + 0.01f);
+            setHealthPercent(1f);
         }
         if (Input.GetKey("down"))
         {
-            setHealthPercent(this.healthPercent - 0.01f);
+            setHealthPercent(this.healthPercent - Random.Range(0.01f,0.03f));
         }
 
         if(currentGhostDelay <= 0 && healthPercent < currentGhostPercent){
@@ -39,6 +40,10 @@ public class HealthBarControl : MonoBehaviour
     }
 
     public void setHealthPercent(float healthPercent){
+        if(healthPercent < 0)
+            healthPercent = 0;
+        if(healthPercent > 1)
+            healthPercent = 1;
         if(this.healthPercent < healthPercent){
             gameObject.transform.Find("Ghost Slider").GetComponent<Slider>().value = healthPercent;
             currentGhostPercent = healthPercent;
@@ -46,7 +51,8 @@ public class HealthBarControl : MonoBehaviour
             
         this.healthPercent = healthPercent;
         gameObject.transform.Find("Main Slider").GetComponent<Slider>().value = this.healthPercent;
-        
+
+        gameObject.transform.Find("TextCmpt").GetComponent<TextMeshProUGUI>().text = (this.healthPercent*100).ToString("0") + "\n%";
         currentGhostDelay = ghostDelay;
     }
 }
