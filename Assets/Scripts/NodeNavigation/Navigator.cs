be6ae330eaac5b0e05e5d349e3b97b13ad8a1313
Jimdooz <romain.saclier@gmail.com>
2020-04-15 21:31:04 +0200

    Ajout de l'arbre d'exploration de niveaux

--- Assets/Scripts/NodeNavigation/Navigator.cs
+++ Assets/Scripts/NodeNavigation/Navigator.cs
@@ -25,8 +25,8 @@ public class Navigator : MonoBehaviour
         if(timerMove > timeMove) timerMove = timeMove;
 
         if(!destinationAcheive && targetNode != null){
-            transform.position = Vector3.Lerp(currentNode.transform.position, targetNode.transform.position, timerMove / timeMove);
-            transform.position = new Vector3(transform.position.x, targetNode.transform.position.y + 1.0f, transform.position.z);
+            transform.position = Vector3.Lerp(currentNode.transform.position, targetNode.transform.position, Mathf.SmoothStep(0.0f, 1.0f, timerMove / timeMove));
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
             if (timerMove != timeMove){
                 transform.LookAt(new Vector3(targetNode.transform.position.x, targetNode.transform.position.y + 1.0f, targetNode.transform.position.z));
                 transform.Rotate(0, -90f, 0);
@@ -41,26 +41,40 @@ public class Navigator : MonoBehaviour
         float movementX = Input.GetAxisRaw("Horizontal");
         float movementY = Input.GetAxisRaw("Vertical");
 
-        if(movementY > 0){
-            if (currentNode.childs[0] && currentNode.childs[0].IsAccessible()) ChangeNode(currentNode.childs[0]);
-        }else if(movementY < 0){
-            if (currentNode.childs[currentNode.childs.Count - 1] && currentNode.childs[currentNode.childs.Count - 1].IsAccessible()) ChangeNode(currentNode.childs[currentNode.childs.Count - 1]);
+        //Si la direction n'est pas nulle alors on se dirige vers la node la plus proche de cette direction pnysiquement
+        if(movementX != 0 || movementY != 0){
+            Vector3 pointDirection = new Vector3(Node.distanceNodes * movementX + transform.position.x, currentNode.transform.position.y, Node.distanceNodes * movementY + transform.position.z);
+            ChangeNode(getNearNode(pointDirection));
         }
 
-        if(movementX > 0){
-            int center = currentNode.childs.Count / 2;
-            if(currentNode.childs.Count + 1 % 2 == 0 && currentNode.childs[center] && currentNode.childs[center].IsAccessible()) ChangeNode(currentNode.childs[center]);
-        }else if(movementX < 0){
-            ChangeNode(currentNode.parent);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentNode.SetComplete(true);
         }
     }
 
     public void ChangeNode(Node target){
-        Debug.Log(target != null);
-        if(target != null && destinationAcheive){
+        if(target != null && destinationAcheive && target.IsAccessible()){
             destinationAcheive = false;
             timerMove = 0f;
             targetNode = target;
         }
+    }
+
+    /**
+     * Permet de récupérer le noeud le plus proche du point projeté
+     */
+    Node getNearNode(Vector3 point){
+        float minDistance = currentNode.parent ? Vector3.Distance(point, currentNode.parent.transform.position) : -1;
+        Node nearNode = currentNode.parent ? currentNode.parent : null;
+        for(int i = 0; i < currentNode.childs.Count; i++){
+            float distanceNode = Vector3.Distance(currentNode.childs[i].transform.position, point);
+            if(distanceNode < minDistance || minDistance == -1){
+                minDistance = distanceNode;
+                nearNode = currentNode.childs[i];
+            }
+        }
+
+        return nearNode;
     }
 }
--- Assets/Scripts/NodeNavigation/Node.cs
+++ Assets/Scripts/NodeNavigation/Node.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    public static int TotalNode = 0;
+    public static float distanceNodes = 10.0f;
     //Représente l'accessibilité d'un noeud
     public bool accessible = false;
+    //Rend le noeud pour toujours innaccessible
+    public bool disabled = false;
     //Représente le fait d'avoir réussi le noeud
     public bool complete = false;
     //Représente le noeud parent
@@ -14,20 +18,32 @@ public class Node : MonoBehaviour
     public List<Node> childs;
 
     protected List<GameObject> constraintsCubes = new List<GameObject>();
+    public float scaleConstraints = 1.0f;
 
     public static GameObject CONSTRAINTS_DESIGN; //Permet d'avoir un objet regroupant toutes les designs contraintes (les cubes qui relient les nodes)
+    public static GameObject NODES_CHILDS; //Permet d'avoir un objet regroupant toutes les designs contraintes (les cubes qui relient les nodes)
 
-    protected Locker lockElement;
+    public Locker lockElement;
+
+    public bool generateTree = false;
+    protected bool alreadyGenerate = false;
+
+    protected Animator animator;
+
+    public float opacity = 1f;
+    public float opacityConstraints = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(CONSTRAINTS_DESIGN == null){
-            CONSTRAINTS_DESIGN = GameObject.Find("CONSTRAINTS_DESIGN");
+        animator = GetComponent<Animator>();
+        if(CONSTRAINTS_DESIGN == null){ CONSTRAINTS_DESIGN = GameObject.Find("CONSTRAINTS_DESIGN"); }
+        if (NODES_CHILDS == null) { NODES_CHILDS = GameObject.Find("NODES_CHILDS"); }
+        if(lockElement == null){
+            lockElement = Instantiate(Resources.Load("Prefabs/UI_3D/Lock") as GameObject).GetComponent<Locker>();
+            lockElement.transform.parent = transform;
+            lockElement.transform.localPosition = new Vector3(0,4.0f,0);
         }
-        lockElement = Instantiate(Resources.Load("Prefabs/UI_3D/Lock") as GameObject).GetComponent<Locker>();
-        lockElement.transform.parent = transform;
-        lockElement.transform.localPosition = new Vector3(0,4.0f,0);
 
         // gameObject.SetActive(IsVisible() || (parent != null && parent.IsVisible()) );
     }
@@ -36,10 +52,15 @@ public class Node : MonoBehaviour
     void Update()
     {
         DrawConstraints();
+        if (generateTree){
+            generateTree = false;
+            GenerateTree();
+        }
     }
 
     void DrawConstraints(){
         if(constraintsCubes.Count != childs.Count ){
+            // if(parent != null) animator.SetTrigger("ConstraintsApparition");
             //Delete before
             for (int i = 0; i < constraintsCubes.Count; i++)
             {
@@ -52,31 +73,74 @@ public class Node : MonoBehaviour
                 cubeConstraint.transform.parent = CONSTRAINTS_DESIGN.transform;
                 cubeConstraint.GetComponent<MeshRenderer>().material = CONSTRAINTS_DESIGN.GetComponent<MeshRenderer>().material;
             }
+            scaleConstraints = 0;
         }
 
         for (int i = 0; i < childs.Count; i++) {
             constraintsCubes[i].transform.position = (transform.position + childs[i].transform.position) / 2;
             constraintsCubes[i].transform.LookAt(transform);
-            constraintsCubes[i].transform.localScale = new Vector3(0.1f, 0.1f, Vector3.Distance(transform.position, childs[i].transform.position) - 1.0f);
+            constraintsCubes[i].transform.localScale = new Vector3(0.1f, 0.1f, Vector3.Distance(transform.position, childs[i].transform.position) - 1.0f) * scaleConstraints;
         }
-        SetOpacity(IsVisible() ? 1.0f : 0.0f);
-        SetOpacityConstraints(IsAccessible() ? 1.0f : 0.0f);
-        lockElement.SetOpacity(!IsAccessible() && IsVisible() ? 1.0f : 0.0f);
+        
+        SetOpacity(opacity);
+        lockElement.SetOpacity(!IsAccessible() ? opacity : 0.0f);
+
+        SetOpacityConstraints(opacityConstraints);
     }
 
-    void SetAccessible(bool a){
+    public void SetAccessible(bool a){
         this.accessible = a;
     }
 
-    void SetComplete(bool c){
+    public void SetComplete(bool c){
+        if(c && !this.complete){
+            for (int i = 0; i < childs.Count; i++)
+            {
+                childs[i].GenerateTree();
+            }
+        }
         this.complete = c;
+        if(parent != null && c) parent.ChildComplete(this);
+    }
+
+    public void ChildComplete(Node childNode){
+        for (int i = 0; i < childs.Count; i++) {
+            if(childNode != childs[i]){
+                childs[i].disabled = true;
+                childs[i].deleteChilds();
+            }
+        }
+    }
+
+    public void deleteChilds(){
+        animator.SetTrigger("ConstraintsDisparition");
+        for (int i = 0; i < childs.Count; i++) {
+            childs[i].deleteChilds();
+            childs[i].DeleteNode();
+        }
+    }
+
+    public void DeleteConstraints(){
+        for (int i = 0; i < constraintsCubes.Count; i++) {
+            Destroy(constraintsCubes[i]);
+        }
+        constraintsCubes.Clear();
+        childs.Clear();
+    }
+
+    public void DeleteNode(){
+        animator.SetTrigger("DIE");
+    }
+
+    public void Die(){
+        Destroy(gameObject);
     }
 
     /**
      * Retourne vrai si le noeud est accessible
      */
     public bool IsAccessible(){
-        return parent == null || parent.IsCompleted();
+        return !disabled && (parent == null || parent.IsCompleted());
     }
 
     /**
@@ -111,5 +175,31 @@ public class Node : MonoBehaviour
                 constraintsRenderer.materials[i].SetColor("_BaseColor", new Color(color.r, color.g, color.b, opacity));
             }
         }
+    }
+
+    public void GenerateTree(int depth = 10){
+        if(depth <= 0 || alreadyGenerate) return;
+        alreadyGenerate = true;
+        int childsNumber = Random.Range(0, 3);
+        for(int i = 0; i <= childsNumber; i++){
+            GameObject nextChild = Instantiate(gameObject);
+            nextChild.name = "NodeChild" + (++TotalNode);
+            Node nextChildNode = nextChild.GetComponent<Node>();
+            nextChildNode.parent = this;
+            nextChild.transform.parent = NODES_CHILDS.transform;
+
+            Vector3 AngleParent = parent != null ? (transform.position - parent.transform.position).normalized : new Vector3();
+            float angle = Mathf.Atan2(AngleParent.z, AngleParent.x) * Mathf.Rad2Deg;
+            nextChild.transform.position += new Vector3(5.0f, 1.0f, (childsNumber / 2f * distanceNodes) - i * distanceNodes).normalized * 5.0f;
+            nextChild.transform.RotateAround(transform.position, Vector3.up, - angle);
+            childs.Add(nextChildNode);
+            nextChildNode.childs.Clear();
+            nextChildNode.SetComplete(false);
+            nextChildNode.scaleConstraints = 0;
+        }
+    }
+
+    public static float CalculateAngle(Vector3 from, Vector3 to) {
+        return Quaternion.FromToRotation(Vector3.up, to - from).eulerAngles.y;
     }
 }
