8df31ddc202e274df95c3b7288791f0347eefc7f
Floran Chazelas <flo.chazelas@orange.fr>
2020-05-04 17:20:24 +0200

    Ajout des models sur ennemies et boss

---
+++ Assets/EnemyList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyList : MonoBehaviour
+{
+    public List<ListEnemyBehavior> list = new List<ListEnemyBehavior>();
+    private int difficulLevel;
+    private List<EntitySpaceShipBehavior> listEnemy = new List<EntitySpaceShipBehavior>();
+
+    public int DifficulLevel { get => difficulLevel; set => difficulLevel = value; }
+    public List<EntitySpaceShipBehavior> ListEnemy { get => listEnemy; set => listEnemy = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CallList();
+    }
+
+    public void CallList()
+    {
+        switch (DifficulLevel)
+        {
+            case 0:
+                ListEnemy = list[0].listEnemy;
+                break;
+            case 1:
+                ListEnemy = list[1].listEnemy;
+                break;
+            case 2:
+                ListEnemy = list[2].listEnemy;
+                break;
+        };
+    }
+}
---
+++ Assets/ListEnemyBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListEnemyBehavior : MonoBehaviour
+{
+    public List<EntitySpaceShipBehavior> listEnemy = new List<EntitySpaceShipBehavior>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
