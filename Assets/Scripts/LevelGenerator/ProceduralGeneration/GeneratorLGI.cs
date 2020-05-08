6590a18b541d8411ebffc7f48c718afb42110cf2
Jimdooz <romain.saclier@gmail.com>
2020-05-08 14:43:26 +0200

    Correction du système de rendu
    Permet d'ajouter de l'anti aliasing

---
+++ Assets/Scripts/LevelGenerator/ProceduralGeneration/GeneratorLGI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Le but de cette Classe est de générer un level de manière correcte selon des contraintes
+ */
+public class GeneratorLGI : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
