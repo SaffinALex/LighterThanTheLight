4843af5e89427510fd38b92c143dde338172528f
Jimdooz <romain.saclier@gmail.com>
2020-05-14 15:04:06 +0200

    Ajout d'armes basiques

---
+++ Assets/Scripts/Entity/Weapon/WeaponsEnemy/ShootAroundEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAroundEnemy : WeaponEnemy
+{
+    [SerializeField] protected float numberAround = 8f;
+    [SerializeField] protected float rotateOverTime = 0f;
+
+    float rotate = 0f;
+    int currentTimeWaveShoot = 0;
+
+    override
+    public void shoot(Transform t)
+    {
+        if (canShoot)
+        {
+            Rigidbody2D playerRgbd = t.gameObject.GetComponent<Rigidbody2D>();
+            int numberShoot = Mathf.RoundToInt(timerShoot / fireRate);
+            Vector2 nextPositionPlayer = playerRgbd.velocity * Time.fixedDeltaTime;
+            float i = 0;
+            for (i = 0; i < numberShoot; i++)
+            {
+                for(int j = 0; j < numberAround; j++){
+                    GameObject o1 = Instantiate(bullet, new Vector3(t.position.x - nextPositionPlayer.x * (i / numberShoot), t.position.y + ((i / numberShoot) * bulletSpeed * Time.fixedDeltaTime) - nextPositionPlayer.y * (i / numberShoot), t.position.z), Quaternion.identity);
+                    o1.GetComponent<Bullet>().setSpeed(bulletSpeed);
+                    o1.GetComponent<Bullet>().setDamage(bulletDamage);
+                    o1.transform.rotation = Quaternion.AngleAxis(j / numberAround * 360 + rotate, Vector3.forward);
+                }
+                rotate += rotateOverTime;
+                currentTimeWaveShoot++;
+                if(currentTimeWaveShoot >= maxNumberWaveShoot - 1){
+                    timerReloadWaveShoot = 0f;
+                    currentTimeWaveShoot = 0;
+                }
+            }
+            reloadShoot();
+        }
+    }
+}
--- Assets/Scripts/Entity/Weapon/WeaponsEnemy/WeaponEnemy.cs
+++ Assets/Scripts/Entity/Weapon/WeaponsEnemy/WeaponEnemy.cs
@@ -12,15 +12,20 @@ public abstract class WeaponEnemy : Weapon
 
     [SerializeField] protected float timeWaitBegin; //Le temps avant que l'arme puisse commencer au début
 
+    [SerializeField] protected int maxNumberWaveShoot = 5;
+    [SerializeField] protected float timeReloadWaveShoot = 5f;
+
     protected bool canShoot;
     protected float timerShoot;
     protected float timerWaitBegin;
+    protected float timerReloadWaveShoot = 0f;
 
     public void Awake() {
         fireRate = fireRate <= minFireRate ? minFireRate : fireRate;
         canShoot = true;
         timerShoot = 0;
         timerWaitBegin = 0;
+        timerReloadWaveShoot = timeReloadWaveShoot;
     }
 
     protected void reloadShoot() {
@@ -30,8 +35,9 @@ public abstract class WeaponEnemy : Weapon
     }
 
     public void updateTimer() {
+        if (timerReloadWaveShoot < timeReloadWaveShoot) { timerReloadWaveShoot += Time.fixedDeltaTime; }
         if (timerWaitBegin < timeWaitBegin) timerWaitBegin += Time.fixedDeltaTime;
         else if (timerShoot <= fireRate) timerShoot += Time.fixedDeltaTime;
-        if (timerShoot >= fireRate) canShoot = true;
+        if (timerShoot >= fireRate && timerReloadWaveShoot >= timeReloadWaveShoot) canShoot = true;
     }
 }
